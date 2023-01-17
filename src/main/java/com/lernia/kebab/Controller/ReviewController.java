3ca484e5febec7e33a0c1e213ab57a155c0f6ee2
Emil Noren <stratosfloor@hotmail.com>
2023-01-17 01:04:34 +0100

    Add endpoints to get just all locations for reviews and all reviews for a certain location

--- src/main/java/com/lernia/kebab/Controller/ReviewController.java
+++ src/main/java/com/lernia/kebab/Controller/ReviewController.java
@@ -1,9 +1,17 @@
 package com.lernia.kebab.Controller;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lernia.kebab.Document.Review;
@@ -19,14 +27,28 @@ public class ReviewController {
     this.reviewRepository = reviewRepository;
   }
 
-  @GetMapping("/reviewssss")
-  public List<Review> getAllReviews2() {
-    return reviewRepository.findAll();
-  }
-
   @GetMapping("/reviews")
   public List<Review> getAllReviews() {
     return reviewRepository.findAllWithOnlyLocation();
   }
-  
+
+  @GetMapping("/reviews/{id}")
+  public Optional<Review> getOneReviewWithId(@PathVariable("id") Long id)  {
+    return reviewRepository.findById(id);
+  }
+
+  @GetMapping("/locations")
+  public Set<GeoJsonPoint> getAllReviewsLocations() {
+     var res = reviewRepository.findAll();
+     final Set<GeoJsonPoint> payload = new HashSet<>();
+     res.forEach(review -> {
+      payload.add(review.getLocation());
+     });
+     return payload;
+  }
+
+  @GetMapping("/locations/reviews")
+  public List<Review> getAllReviewsForLocation(@RequestBody GeoJsonPoint point)  {
+    return reviewRepository.findAllReviewsForLocation(point);
+  }
 }
--- src/main/java/com/lernia/kebab/Repository/ReviewRepository.java
+++ src/main/java/com/lernia/kebab/Repository/ReviewRepository.java
@@ -2,6 +2,7 @@ package com.lernia.kebab.Repository;
 
 import java.util.List;
 
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
@@ -10,7 +11,13 @@ import com.lernia.kebab.Document.Review;
 public interface ReviewRepository extends MongoRepository<Review, Long>{
 
   
+  // @Query(value="{}",  fields="{ location : 1 }")
+  // List<Review> findAllWithOnlyLocation();
+
   @Query(value="{}",  fields="{ location : 1 }")
   List<Review> findAllWithOnlyLocation();
+
+  @Query(value="{ location: ?0 } ")
+  List<Review> findAllReviewsForLocation(GeoJsonPoint point);
   
 }
